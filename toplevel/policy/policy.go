5337bdb426343a3aded2a02aab739f5c9efc3702
Roman Iuvshyn <riuvshyn@redhat.com>
2019-03-15 15:49:57 +0200

    clean up

--- toplevel/policy/policy.go
+++ toplevel/policy/policy.go
@@ -3,12 +3,10 @@
 package policy
 
 import (
-	"fmt"
 	"github.com/app-sre/vault-manager/pkg/vault"
 	"github.com/app-sre/vault-manager/toplevel"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
-	"time"
 )
 
 type config struct{}
@@ -40,19 +38,18 @@ func (e entry) Equals(i interface{}) bool {
 }
 
 func (c config) Apply(entriesBytes []byte, dryRun bool) {
-	fmt.Println("policy start:",time.Now().Format(time.Stamp))
 	// Unmarshal the list of configured secrets engines.
 	var entries []entry
 	if err := yaml.Unmarshal(entriesBytes, &entries); err != nil {
 		log.WithField("package", "policy").WithError(err).Fatal("failed to decode policies configuration")
 	}
-	fmt.Println("policy after unmarshal:",time.Now().Format(time.Stamp))
+
 	// List the existing policies.
 	existingPolicyNames, err := vault.Client().Sys().ListPolicies()
 	if err != nil {
 		log.WithField("package", "policy").WithError(err).Fatal("failed to list policies from Vault instance")
 	}
-	fmt.Println("policy after existing policies:",time.Now().Format(time.Stamp))
+
 	// Build a list of all the existing entries.
 	existingPolicies := make([]entry, 0)
 	if existingPolicies != nil {
@@ -64,10 +61,9 @@ func (c config) Apply(entriesBytes []byte, dryRun bool) {
 			existingPolicies = append(existingPolicies, entry{Name: name, Rules: policy})
 		}
 	}
-	fmt.Println("policy after build existing policies list:",time.Now().Format(time.Stamp))
 	// Diff the local configuration with the Vault instance.
 	toBeWritten, toBeDeleted := vault.DiffItems(asItems(entries), asItems(existingPolicies))
-	fmt.Println("policy after diff:",time.Now().Format(time.Stamp))
+
 	if dryRun == true {
 		for _, w := range toBeWritten {
 			log.WithField("package", "policy").Infof("[Dry Run] policy to be written='%v'", w.Key())
@@ -102,7 +98,6 @@ func (c config) Apply(entriesBytes []byte, dryRun bool) {
 			log.WithField("package", "policy").WithField("name", ent.Name).Info("successfully deleted policy from Vault instance")
 		}
 	}
-	fmt.Println("policy finish:",time.Now().Format(time.Stamp))
 }
 
 func isDefaultPolicy(name string) bool {
