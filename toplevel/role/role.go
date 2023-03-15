160d4a9f1f56722c9aece18a291048990fef2ee3
Drew Welch <dwelch@redhat.com>
2023-03-15 09:22:13 -0500

    remove deprecated functionality

--- toplevel/role/role.go
+++ toplevel/role/role.go
@@ -162,10 +162,6 @@ func (c config) Apply(address string, entriesBytes []byte, dryRun bool, threadPo
 	}
 
 	addOptionalOidcDefaults(address, instancesToDesiredRoles[address])
-	err = pruneUnsupported(address, instancesToDesiredRoles[address])
-	if err != nil {
-		return err
-	}
 
 	err = unmarshallOptionObjects(instancesToDesiredRoles[address])
 	if err != nil {
@@ -291,26 +287,4 @@ func addOptionalOidcDefaults(instance string, roles []entry) {
 			}
 		}
 	}
-}
-
-// remove attributes not supported in commercial but in fedramp variant
-func pruneUnsupported(instance string, roles []entry) error {
-	ver, err := vault.GetVaultVersion(instance)
-	if err != nil {
-		return err
-	}
-	current, err := version.NewVersion(ver)
-	if err != nil {
-		return err
-	}
-	// https://github.com/hashicorp/vault/blob/main/CHANGELOG.md#170
-	threshold, err := version.NewVersion("1.7.0")
-	if current.LessThan(threshold) {
-		for _, role := range roles {
-			if strings.ToLower(role.Type) == "oidc" {
-				delete(role.Options, "max_age")
-			}
-		}
-	}
-	return nil
 }
