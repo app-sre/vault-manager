d1c7ec885609d611e0a4d5abf0f65be920b7e8e0
Jon Mosco <jmosco@redhat.com>
2025-08-07 08:09:30 -0400

    try running unit tests only

--- pr_check.sh
+++ pr_check.sh
@@ -3,7 +3,9 @@
 # compile sources and run unit tests
 make build
 
-# run integration tests
-source .env
-# make gobuild
-make test-testcontainers-shared
+# run main application tests (unit tests)
+make gotest
+
+# Note: Testcontainers integration tests are skipped in CI due to 
+# container networking restrictions. Run locally with:
+# make test-testcontainers-shared
